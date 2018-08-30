cbadb526ddee59af5fbfe4cb3e12bd3e2e040996
Richard Fung <richardfung@google.com>
2018-08-29 17:43:23 -0700

    Add filter for month to boa_checking

--- finances.py
+++ finances.py
@@ -1,10 +1,13 @@
 import csv
 
-def boa_checking(input_filename, output_filename):
+def boa_checking(input_filename, output_filename, month):
     """Format is some stuff we don't care about for first n lines, then a blank
     line, and then the csv stuff we are interested in.
 
     The csv has Date, Description, Amount, Total Balance"""
+
+    def test(xs):
+        return xs[0].startswith(month) or xs[0].startswith('0%s' % month)
 
     def transform(xs):
         return xs[0:3]
@@ -27,4 +30,5 @@ def boa_checking(input_filename, output_filename):
         with open(output_filename, 'wb') as out_file:
             writer = csv.writer(out_file)
             for row in reader:
-                writer.writerow(transform(row))
+                if test(row):
+                    writer.writerow(transform(row))
