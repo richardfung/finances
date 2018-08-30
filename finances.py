8d7e4b27204d5ecb6add37da4291a55399307546
Richard Fung <richardfung@google.com>
2018-08-29 17:38:53 -0700

    Add boa_checking for checking account

---
+++ finances.py
@@ -0,0 +1,30 @@
+import csv
+
+def boa_checking(input_filename, output_filename):
+    """Format is some stuff we don't care about for first n lines, then a blank
+    line, and then the csv stuff we are interested in.
+
+    The csv has Date, Description, Amount, Total Balance"""
+
+    def transform(xs):
+        return xs[0:3]
+
+    with open(input_filename, 'rb') as in_file:
+        reader = csv.reader(in_file)
+        # Skip past the first blank line
+        while reader.next():
+            pass
+        # Remove the description line
+        if reader.next() != ['Date', 'Description', 'Amount', 'Running Bal.']:
+            raise ValueError('Expected description line. ' +
+                             'Did the format change?')
+
+        # Remove the "beginning balance" line
+        if not reader.next()[1].startswith('Beginning balance as of'):
+            raise ValueError('Expected beginning balance line. ' +
+                             'Did the format change?')
+
+        with open(output_filename, 'wb') as out_file:
+            writer = csv.writer(out_file)
+            for row in reader:
+                writer.writerow(transform(row))
